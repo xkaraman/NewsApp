b32fd641d157279ef8960bb669fd3186e540c65c
xkaraman <xenakiskaramanos@gmail.com>
2018-06-01 20:12:25 +0300

    News App V2 Menu Inflation

--- app/src/main/java/com/example/android/newsapp/ArticleActivity.java
+++ app/src/main/java/com/example/android/newsapp/ArticleActivity.java
@@ -9,7 +9,8 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -97,6 +98,25 @@ public class ArticleActivity extends AppCompatActivity
             mEmptyStateTextView.setText(R.string.no_internet_connection);
 
         }
+    }
+
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
